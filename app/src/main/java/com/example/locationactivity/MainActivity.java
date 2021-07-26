cbfb66066d2442dd696077c30e48dedf4afd3647
Halim Ratmono <hratmono@uwaterloo.ca>
2021-07-26 00:10:47 -0400

    Added Bluetooth

--- app/src/main/java/com/example/locationactivity/MainActivity.java
+++ app/src/main/java/com/example/locationactivity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.locationactivity;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.location.Address;
@@ -23,19 +27,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
 
+import android.os.Handler;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final int DEFAULT_UPDATE_INTERVAL = 1;//30
@@ -66,6 +77,24 @@ public class MainActivity extends AppCompatActivity {
     public ArrayList<Datapoint> datapoints;
     DatabaseHelper myDB;
     int id_test = 0;
+
+    //Bluetooth
+    private final String DEVICE_ADDRESS="00:14:03:06:92:C3";
+    private final UUID PORT_UUID = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");//Serial Port Service ID
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+    private OutputStream outputStream;
+    private InputStream inputStream;
+    Button startButton,clearButton,stopButton;//sendButton
+    TextView textView;
+    EditText editText;
+    boolean deviceConnected=false;
+    boolean seeData=false;
+    Thread thread;
+    byte buffer[];
+    int bufferPosition;
+    boolean stopThread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -163,6 +192,17 @@ public class MainActivity extends AppCompatActivity {
         myDB = new DatabaseHelper(this);
 //        datapoints=getAll(datapoints);
         //^SQL management
+
+
+        //BLUETOOTH UI v//////////////////
+        startButton = (Button) findViewById(R.id.start);
+//        sendButton = (Button) findViewById(R.id.buttonSend);
+        clearButton = (Button) findViewById(R.id.clear);
+        stopButton = (Button) findViewById(R.id.stop);
+//        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.bluetooth_text);
+        setUiEnabled(false);
+        //BLUETOOTH UI ^//////////////////
     }
 
     private void startLocationUpdates() {
@@ -307,7 +347,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    //SQL METHODS
+    //SQL METHODS//////////////////////////////////
     public void DeleteData(int mId){
         Integer deletedRows = myDB.deleteData(Integer.toString(mId));
         if (deletedRows>0){
@@ -381,5 +421,209 @@ public class MainActivity extends AppCompatActivity {
         }
         return patientList;
     }
-    //^SQL METHODS
+    //^SQL METHODS//////////////////////////////////
+
+
+    //BLUETOOTH v/////////////////////////////////
+
+    public void setUiEnabled(boolean bool)
+    {
+//        this.startButton.setEnabled(!bool);
+////        sendButton.setEnabled(bool);
+//        this.stopButton.setEnabled(bool);
+//        this.textView.setEnabled(bool);
+    }
+
+    public boolean BTinit()
+    {
+        boolean found=false;
+        BluetoothAdapter bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Device doesnt Support Bluetooth",Toast.LENGTH_SHORT).show();
+        }
+        if(!bluetoothAdapter.isEnabled())
+        {
+            Intent enableAdapter = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableAdapter, 0);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Set<BluetoothDevice> bondedDevices = bluetoothAdapter.getBondedDevices();
+        if(bondedDevices.isEmpty())
+        {
+            Toast.makeText(getApplicationContext(),"Please Pair the Device first",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            for (BluetoothDevice iterator : bondedDevices)
+            {
+                if(iterator.getAddress().equals(DEVICE_ADDRESS))
+                {
+                    device=iterator;
+                    found=true;
+                    break;
+                }
+            }
+        }
+        return found;
+    }
+
+    public boolean BTconnect()
+    {
+        boolean connected=true;
+        try {
+            socket = device.createRfcommSocketToServiceRecord(PORT_UUID);
+            socket.connect();
+        } catch (IOException e) {
+            e.printStackTrace();
+            connected=false;
+        }
+        if(connected)
+        {
+            try {
+                outputStream=socket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                inputStream=socket.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        return connected;
+    }
+
+    public void onClickStart(View view) {
+        if(BTinit())
+        {
+            if(BTconnect())
+            {
+                setUiEnabled(true);
+                deviceConnected=true;
+                beginListenForData();
+
+//                String string2 = "r";
+//                    string2.concat("\n");
+//                try {
+//                 outputStream.write(string2.getBytes());
+//                } catch (IOException e) {
+//                  e.printStackTrace();
+//                }
+
+                textView.append("\nConnection Opened!\n");
+
+            }
+
+        }
+    }
+
+    void beginListenForData()
+    {
+        final Handler handler = new Handler();
+        stopThread = false;
+        buffer = new byte[1024];
+        Thread thread  = new Thread(new Runnable()
+        {
+            public void run()
+            {
+                while(!Thread.currentThread().isInterrupted() && !stopThread)
+                {
+                    try
+                    {
+                        final int byteCount = inputStream.available();
+                        if(byteCount > 9)
+                        {
+                            byte[] rawBytes = new byte[byteCount];
+                            inputStream.read(rawBytes);
+                            final String string=new String(rawBytes,"UTF-8");
+
+                            final int dataLength = string.length();
+                            if (dataLength == 10)
+                            {
+                                seeData = true;
+                            }
+                            final String temp =new String(string.substring(0,5));
+                            final String humid =new String(string.substring(5,10));
+                            final Float temp3 = Float.parseFloat(temp);
+                            final Float humid3 = Float.parseFloat(humid);
+
+//                            byte[] tempNum = Arrays.copyOfRange(rawBytes,0,5 );
+//                            byte[] humidNum = Arrays.copyOfRange(rawBytes,5,10 );
+//                            final String temp2 =new String(tempNum,"UTF-8");
+//                            final String humid2 =new String(humidNum,"UTF-8");
+
+//                           final String temp4 =new String(string.substring(0,1));
+
+                            handler.post(new Runnable() {
+                                public void run()
+                                {
+//                                    textView.append(String.valueOf(dataLength));
+//                                    textView.setText("");
+
+//                                    if (seeData) {
+//                                        textView.append(temp2);
+//                                        seeData = false;
+//                                    }
+
+                                    textView.append ("Temp = ");
+                                    textView.append(String.valueOf(temp3));
+                                    textView.append (" Humidity = ");
+                                    textView.append(String.valueOf(humid3));
+                                    if (humid3>50){
+                                        textView.append (" Too High!");
+                                    }
+                                    textView.append ("\n");
+                                }
+                            });
+
+
+                        }
+                    }
+                    catch (IOException ex)
+                    {
+                        stopThread = true;
+                    }
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+//    public void onClickSend(View view) {
+//        String string = editText.getText().toString();
+//        string.concat("\n");
+//          String string2 = ("r");
+//            string.concat("\n");
+//        try {
+//            outputStream.write(string.getBytes());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        textView.append("\nMonitor Start:"+string+"\n");
+//
+//    }
+
+    public void onClickStop(View view) throws IOException {
+        stopThread = true;
+        outputStream.close();
+        inputStream.close();
+        socket.close();
+        setUiEnabled(false);
+        deviceConnected=false;
+        textView.append("\nConnection Closed!\n");
+    }
+
+    public void onClickClear(View view) {
+        textView.setText("");
+    }
+
+    //BLUETOOTH ^/////////////////////////////////
 }
